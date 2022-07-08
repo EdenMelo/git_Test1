4461c3bfcc6fb05da9b22dd98bac8462d78db923
Melo <menglongde777@outlook.com>
2022-07-08 18:01:14 +0800

    gitee commit

--- src/main/java/com/melo/git/GitTest.java
+++ src/main/java/com/melo/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hot-fix git!");
         System.out.println("push git!");
         System.out.println("push git2!");
+        System.out.println("Gitee push test!");
     }
 }
